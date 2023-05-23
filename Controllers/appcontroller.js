1d31de111890885c05468208c47bb34fef401544
Shubham Kaushik <shubhamkaushik00009@gmail.com>
2023-05-24 00:28:30 +0530

    edit

--- Controllers/appcontroller.js
+++ Controllers/appcontroller.js
@@ -133,7 +133,7 @@ const vote = async (req, res, next) => {
 
   try {
     await client.query(
-      "UPDATE users isvoted=true , isvotednow = 1 , partyId= $2  WHERE fid=$1",
+      "UPDATE users set isvoted=true , isvotednow = 1 , partyId= $2  WHERE fid=$1",
       [fid, partyId]
     );
   } catch (err) {
